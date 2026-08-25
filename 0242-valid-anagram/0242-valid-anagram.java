650f2fdcb2a68f5fcf16cef6576df24648cd8da8
mansiWagh88 <mansiwagh2611@gmail.com>
2026-08-25 23:45:32 +0530

    Time: 5 ms (62.25%), Space: 44.1 MB (95.18%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        int n=s.length();
+        int[] freqS=new int[26];
+        int[] freqT=new int[26];
+        for(int i=0;i<n;i++){
+            freqS[s.charAt(i)-'a']++;
+            freqT[t.charAt(i)-'a']++;
+        }
+        for(int i=0;i<26;i++){
+            if(freqS[i] != freqT[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
