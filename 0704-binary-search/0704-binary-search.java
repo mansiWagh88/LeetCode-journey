c367c4def04e20c50f323738a56f2e72dec2e501
mansiWagh88 <mansiwagh2611@gmail.com>
2026-09-01 23:31:11 +0530

    Time: 0 ms (100%), Space: 48.5 MB (14.01%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,13 +1,13 @@
 class Solution {
     public int search(int[] nums, int target) {
-    int n=nums.length;
+        int n=nums.length;
         return func(nums,0,n-1,target);
     }
-    private int func(int[]nums,int low,int high,int target){
+    private int func(int[]nums, int low,int high,int target){
         if(low>high) return -1;
         int ind;
         int mid=(low+high)/2;
-        if(nums[mid]==target)  ind=mid;
+        if(nums[mid]==target) ind=mid;
         else if(nums[mid]>target) ind=func(nums,low,mid-1,target);
         else ind=func(nums,mid+1,high,target);
         return ind;
